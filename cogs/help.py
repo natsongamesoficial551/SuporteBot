05ccc4a70b6314b0517703ac224f5eb4fcbecf98
natsongamesoficial551 <borgesnatan09@gmail.com>
2025-06-21 10:26:19 -0300

    Adicionando sistema de Economia + Atualizando o Help com todos os comandos

--- cogs/help.py
+++ cogs/help.py
@@ -5,7 +5,7 @@ class Help(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='ajuda', aliases=['help'])
+    @commands.command(name='ajuda', aliases=['ajuda'])
     async def ajuda(self, ctx):
         embed = discord.Embed(
             title="📜 Lista de Comandos do Bot",
